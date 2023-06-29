df40f2012a134c3abe4e6ccf9a5d10d9f56b5c1f
dedoyin21 <adediranadedoyin@gmail.com>
2023-06-29 10:27:02 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,20 +7,18 @@
  * @n: input value
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, const char *src, int n) 
 {
-	int j;
+	
+	int i;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++) 
 	{
-		dest[j] - src[j];
-		j++
+		dest[i] = src[i];
 	}
-	while (j < n)
+	for (; i < n; i++) 
 	{
-		dest[j] = '\0';
-		j++
+		dest[i] = '\0';
 	}
-	return (dest);
+	return dest;
 }
