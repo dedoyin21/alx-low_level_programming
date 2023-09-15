cdabad094e59ddbb59edcbf69a4f6fd35edff090
dedoyin21 <adedirandedoyin.com>
2023-09-15 03:57:14 -0700

    initial commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,28 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - sum of variadic variable
- * @n:const unsigned
- * Return:int n
+ * sum_them_all - the sum of variadic variable
+ * @n:constant
+ * Return:integer
  */
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-va_list list;
-double sum = 0;
+	unsigned int i;
+	va_list list;
+	double sum = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
-va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(list, int);
-	}
+		if (n == 0)
+		{
+			return (0);
+		}
+		if (n == 0)
+		{
+			return (0);
+		}
+	va_start(list, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(list, int);
+		}
 	va_end(list);
 	return (sum);
 }
